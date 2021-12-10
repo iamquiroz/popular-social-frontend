9f63eb1abb0febcac54b2c67565a46b9429dc9f3
Josh W <jquiroz.mexicali@gmail.com>
2021-12-10 02:13:48 -0800

    bracket

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Stories from "./Stories";
 import styled from "styled-components";
+import Messenger from './Messenger';
 const Feed = () => {
   return (
     <FeedWrapper>
       <Stories />
+      <Messenger />
     </FeedWrapper>
   );
 };
---
+++ src/components/Messenger.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Avatar, Input } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import styled from "styled-components";
+
+
