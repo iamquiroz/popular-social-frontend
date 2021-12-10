2bc36b298d73f20714561e4c9221258b445fe566
Josh W <jquiroz.mexicali@gmail.com>
2021-12-09 23:43:44 -0800

    Header ready

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHPxkCrF-vm7pSM42_ubw7jhrYqC8OyBg",
+
+  authDomain: "popular-social-67bb6.firebaseapp.com",
+
+  projectId: "popular-social-67bb6",
+
+  storageBucket: "popular-social-67bb6.appspot.com",
+
+  messagingSenderId: "27630493201",
+
+  appId: "1:27630493201:web:f25ec67e60c3e2fd90dbb0",
+
+  measurementId: "${config.measurementId}",
+};
